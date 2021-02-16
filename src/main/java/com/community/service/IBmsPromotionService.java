5556cc4c4e8f826b21dd17bcecbfe37cca2ad500
KyrieFeng <1551246401@qq.com>
2021-02-16 15:10:56 +0800

    推广功能

---
+++ src/main/java/com/community/service/IBmsPromotionService.java
@@ -0,0 +1,7 @@
+package com.community.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.community.model.entity.BmsPromotion;
+
+public interface IBmsPromotionService extends IService<BmsPromotion> {
+}
